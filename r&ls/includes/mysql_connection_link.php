13c0a923f9e2e12ed6ab991c74b0e8dd182d65ac
NexLFC <45148835+NexLFC@users.noreply.github.com>
2019-02-08 13:32:06 +0100

    .

--- r&ls/includes/mysql_connection_link.php
+++ r&ls/includes/mysql_connection_link.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_USER','root');
-define('DB_PASSWORD','Admin1234!@#');
-define('DB_HOST','http://192.168.33.10');
+define('DB_PASSWORD','5k09gj3-od2kfi');
+define('DB_HOST','http://46.101.131.157');
 define('DB_NAME','r&ls');
 
 $dbcl = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
--- r&ls/logout.php
+++ r&ls/logout.php
@@ -4,9 +4,9 @@ if(isset($_SESSION['user_id'])){
     $_SESSION = array();
     session_destroy();
     setcookie(session_name(),'',time()-3600);
-    header('Location:http://192.168.33.10/r&ls/');
+    header('Location:http://46.101.131.157/r&ls/');
     exit();
 }else{
-    header('Location:http://192.168.33.10/r&ls/');
+    header('Location:http://46.101.131.157/r&ls/');
     exit();
 }
